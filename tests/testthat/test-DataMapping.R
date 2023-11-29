fbcbd4865d870ba48b119f2e41c526ca09af475a
Katrin Coboeken <katrin.coboeken@bayer.com>
2023-11-29 08:05:37 +0100

    switch for shape mode

--- tests/testthat/test-DataMapping.R
+++ tests/testthat/test-DataMapping.R
@@ -270,6 +270,7 @@ test_that("adjust secondary y axis scaling works log to lin", {
 test_that("grouping for simulation and observed works", {
   simData <- exampleDataTimeProfile %>%
     dplyr::filter(Type == "simulated") %>%
+    dplyr::filter(SetID %in%  c('DataSet1','DataSet2','DataSet3')) %>%
     dplyr::select(c("time", "values", "minValues", "maxValues", "caption", "dimension"))
 
 
@@ -297,7 +298,7 @@ test_that("grouping for simulation and observed works", {
     expected = "groupBy.i"
   )
 
-
+ # group or group by is missing
   expect_error(
     simMappedData <- MappedDataTimeProfile$new(
       data = simData,
@@ -318,7 +319,7 @@ test_that("grouping for simulation and observed works", {
 
 
   simMappedData <- MappedDataTimeProfile$new(
-    data = simData,
+    data = simData[],
     mapping = mapping <- aes(
       x = time,
       y = values,

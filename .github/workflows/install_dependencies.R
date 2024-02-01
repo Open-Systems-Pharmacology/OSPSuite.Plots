d8d7cc7e8bef3689952754674f31eec977e35136
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2024-02-01 21:57:46 +0800

    Delete .github/workflows directory

--- .github/workflows/install_dependencies.R
+++
@@ -1,4 +0,0 @@
-install.packages(c('testthat','rmarkdown','spelling','ggplot2','ggh4x','data.table','ggnewscale','checkmate','scales','dplyr','tidyr'), repos = 'http://cran.us.r-project.org', type='win.binary')
-download.file('https://ci.appveyor.com/api/projects/open-systems-pharmacology-ci/ospsuite-rutils/artifacts/ospsuite.utils.zip?pr=false', destfile = 'ospsuite.utils.zip', mode='wb')
-install.packages('ospsuite.utils.zip', repos = NULL, type = 'binary')
-unlink('ospsuite.utils.zip')

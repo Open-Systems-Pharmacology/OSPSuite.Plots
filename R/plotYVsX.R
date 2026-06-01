d7e4316340b37422cf248e055fb18fde8a2f57cb
Felix MIL <dev@felixmil.com>
2026-06-01 10:20:57 +0200

    Fix swapped errorbar orientation in plotYVsX
    - R/plotYVsX.R:
      - pass orientation "x" for the xmin/xmax branch and "y" for the ymin/ymax branch so geom_errorbar_osp reads the mapped range columns instead of the missing opposite-axis ones
    - tests/testthat/test-plotYVsX.R:
      - add vdiffr snapshots for single-axis error bars in both directions
    - tests/testthat/_snaps/plotYVsX/res.svg:
      - regenerate after orientation fix
    - tests/testthat/_snaps/plotYVsX/xdir-errorbars.svg:
      - add snapshot showing horizontal error bars on the x-axis
    - tests/testthat/_snaps/plotYVsX/ydir-errorbars.svg:
      - add snapshot showing vertical error bars on the y-axis

--- R/plotYVsX.R
+++ R/plotYVsX.R
@@ -30,7 +30,8 @@ plotResVsCov <- function(
     warning(messages$warningResidualScaleDeprecated())
   }
 
-  if (!is.null(yScale)) yScale <- match.arg(yScale, c(AxisScales$linear, AxisScales$log))
+  if (!is.null(yScale))
+    yScale <- match.arg(yScale, c(AxisScales$linear, AxisScales$log))
 
   plotObject <- plotYVsX(
     data = data,
@@ -78,8 +79,10 @@ plotRatioVsCov <- function(
     warning(messages$warningResidualScaleDeprecated())
   }
 
-  if (!is.null(xScale)) xScale <- match.arg(xScale, c(AxisScales$linear, AxisScales$log))
-  if (!is.null(yScale)) yScale <- match.arg(yScale, c(AxisScales$linear, AxisScales$log))
+  if (!is.null(xScale))
+    xScale <- match.arg(xScale, c(AxisScales$linear, AxisScales$log))
+  if (!is.null(yScale))
+    yScale <- match.arg(yScale, c(AxisScales$linear, AxisScales$log))
 
   yDisplayAsAbsolute <- FALSE
 
@@ -217,8 +220,10 @@ plotYVsX <- function(
   if (is.double(comparisonLineVector)) {
     comparisonLineVector <- as.list(comparisonLineVector)
   }
-  if (!is.null(xScale)) xScale <- match.arg(xScale, c(AxisScales$linear, AxisScales$log))
-  if (!is.null(yScale)) yScale <- match.arg(yScale, c(AxisScales$linear, AxisScales$log))
+  if (!is.null(xScale))
+    xScale <- match.arg(xScale, c(AxisScales$linear, AxisScales$log))
+  if (!is.null(yScale))
+    yScale <- match.arg(yScale, c(AxisScales$linear, AxisScales$log))
   .validatePlotYXsXInputs(
     data = data,
     metaData = metaData,
@@ -292,7 +297,7 @@ plotYVsX <- function(
         args = utils::modifyList(
           x = list(
             na.rm = TRUE,
-            orientation = "y"
+            orientation = "x"
           ),
           val = geomErrorbarAttributes
         )
@@ -306,7 +311,7 @@ plotYVsX <- function(
         args = utils::modifyList(
           list(
             na.rm = TRUE,
-            orientation = "x"
+            orientation = "y"
           ),
           geomErrorbarAttributes
         )
